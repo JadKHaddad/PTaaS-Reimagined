fa37f01c521afb3b4a193552a5412f5f9fee9bbf
JadKHaddad <jadkhaddad@gmail.com>
2023-07-12 21:39:58 +0200

    Skel.

--- ptaas_rs/src/main.rs
+++ ptaas_rs/src/main.rs
@@ -1,9 +1,46 @@
-use ptaas_rs::models::{
-    APIGerneralResponseErrorType, APIResponse, APIResponseError, APIResponseType,
-    AllProjectsResponseData, AllProjectsResponseErrorType, Project, Script,
+use ptaas_rs::{
+    models::{
+        APIGerneralResponseErrorType, APIResponse, APIResponseError, APIResponseType,
+        AllProjectsResponseData, AllProjectsResponseErrorType, Project, Script,
+    },
+    project_managers::LocalProjectManager,
 };
+use tracing_subscriber::EnvFilter;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "ptaas_rs=trace,tower_http=off,hyper=off");
+    }
+
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .with_timer(tracing_subscriber::fmt::time::UtcTime::rfc_3339())
+        .with_level(true)
+        .with_ansi(true)
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    let basic_auth_username = std::env::var("BASIC_AUTH_USERNAME").unwrap_or_else(|_| {
+        tracing::warn!("BASIC_AUTH_USERNAME not set, using default value");
+        String::from("admin")
+    });
+    let basic_auth_password = std::env::var("BASIC_AUTH_PASSWORD").unwrap_or_else(|_| {
+        tracing::warn!("BASIC_AUTH_PASSWORD not set, using default value");
+        String::from("admin")
+    });
+
+    let root_dir = "./projects";
+    let manager = match LocalProjectManager::new(root_dir.into()).await {
+        Ok(manager) => manager,
+        Err(error) => {
+            tracing::error!(%error, "Failed to create LocalProjectManager");
+            std::process::exit(1);
+        }
+    };
+}
+
+fn create_dummy() {
     // Create fake data for an API response indicating success
     let projects = vec![
         Project {
---
+++ ptaas_rs/src/project_managers/local_project_manager/local_project_manager.rs
@@ -0,0 +1,30 @@
+use std::path::PathBuf;
+use thiserror::Error as ThisError;
+use tokio::fs;
+pub struct LocalProjectManager {
+    root_dir: PathBuf,
+}
+
+#[derive(ThisError, Debug)]
+pub enum LocalProjectManagerCreateError {
+    #[error("Could check if root dir exists: {0}")]
+    CouldNotCheckIfRootDirExists(#[source] std::io::Error),
+    #[error("Could not create root dir: {0}")]
+    CouldNotCreateRootDir(#[source] std::io::Error),
+}
+
+impl LocalProjectManager {
+    pub async fn new(root_dir: PathBuf) -> Result<Self, LocalProjectManagerCreateError> {
+        if !fs::try_exists(&root_dir)
+            .await
+            .map_err(|e| LocalProjectManagerCreateError::CouldNotCheckIfRootDirExists(e))?
+        {
+            tracing::info!(?root_dir, "Root dir does not exist, creating it");
+            fs::create_dir_all(&root_dir)
+                .await
+                .map_err(|e| LocalProjectManagerCreateError::CouldNotCreateRootDir(e))?;
+        }
+
+        Ok(Self { root_dir })
+    }
+}
